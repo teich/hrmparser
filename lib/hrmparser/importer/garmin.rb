a194feb889bf2d2ad10a46fbb82b848ca09cd6ea
Oren Teich <oren@teich.net>
2009-05-06 13:39:28 -0700

    Handle missing trackpoints and missing HR

--- lib/hrmparser/importer/garmin.rb
+++ lib/hrmparser/importer/garmin.rb
@@ -24,26 +24,32 @@ module Importer
       (@xml/:Trackpoint).each do |t| 
         found = true
         trackpoint = HRMParser::TrackPoint.new
-        next if ((t/:HeartRateBpm/:Value).innerHTML == "")
-        trackpoint.hr = (t/:HeartRateBpm/:Value).innerHTML.to_i
+
         trackpoint.time = Time.parse((t/:Time).innerHTML)
-        trackpoint.altitude = (t/:AltitudeMeters).innerHTML.to_f
-        trackpoint.distance = (t/:DistanceMeters).innerHTML.to_f
+
+        hr = (t/:HeartRateBpm/:Value).innerHTML
+        alt = (t/:AltitudeMeters).innerHTML
+        dis = (t/:DistanceMeters).innerHTML
+        
+        trackpoint.hr =       hr != "" ? hr.to_i : nil
+        trackpoint.altitude = alt != "" ? alt.to_f : nil
+        trackpoint.distance = dis != "" ? dis.to_f : nil
 
         (t/:Position).each do |p|
-          trackpoint.lat = (p/:LatitudeDegrees).innerHTML.to_f
-          trackpoint.lng = (p/:LongitudeDegrees).innerHTML.to_f
+          trackpoint.lat = (p/:LatitudeDegrees).innerHTML
+          trackpoint.lng = (p/:LongitudeDegrees).innerHTML
         end
-
 
         trackpoints << trackpoint
         
     
+        ## CALCULATE SPEED.  ICK.
         if distance_one.nil?
           distance_one = trackpoint.distance
           time_one = trackpoint.time
         else
           distance_two = trackpoint.distance
+          next if distance_two.nil?
           time_two = trackpoint.time
           time_delta = time_two - time_one
           distance_delta = distance_two - distance_one
@@ -51,17 +57,25 @@ module Importer
             trackpoint.speed = distance_delta / time_delta  
             distance_one = distance_two
             time_one = time_two
-          else trackpoint.speed = nil
+          else 
+            trackpoint.speed = nil
           end      
         end
       end 
-      
+
       if found
         workout.trackpoints = trackpoints
-        workout.distance = trackpoints.last.distance if !trackpoints.last.distance.nil?
         workout.calc_average_speed! 
         workout.calc_altitude_gain!
         workout.calc_average_hr!
+        trackpoints.reverse_each do |tp|
+          if !tp.distance.nil?
+            workout.distance = tp.distance
+            break
+          end
+        end
+        # workout.distance = trackpoints.last.distance if !trackpoints.last.nil? && !trackpoints.last.distance.nil?
+
       end
 
       return workout
--- spec/hrmparser_spec.rb
+++ spec/hrmparser_spec.rb
@@ -3,130 +3,142 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 # Time to add your specs!
 # http://rspec.info/
 module HRMParser
-    describe "TrackPoint" do
-      context "new trackpoint" do
-        it "has no variables set" do
-          hrm = TrackPoint.new
-          hrm.speed == nil
-          hrm.distance == nil
-          hrm.lat.should == nil
-          hrm.lng.should == nil
-          hrm.altitude.should == nil
-          hrm.hr.should == nil
-        end
-      end
-    end
-    
-    describe "Workout" do
-      context "new workout" do
-        it "has no variables set" do
-          workout = Workout.new
-          workout.distance.should == nil
-          workout.duration.should == nil
-          workout.average_hr.should == nil
-          workout.name.should == nil
-          workout.file_name.should == nil
-        end
-        it "set name through initializer" do
-          workout = Workout.new(:name => "test workout")
-          workout.name.should == "test workout"
-        end
-        it "can not set average_hr during init" do
-          workout = Workout.new(:average_hr => 150)
-          workout.average_hr.should == nil
-        end
-      end
-      
-      context "Identifies files" do
-        it "identify file as garmin" do
-          type = Importer.file_type("spec/samples/small-garmin.TCX")
-          type.should == "GARMIN_XML"
-        end
-        it "identification returns nil if no file specified" do
-          type = Importer.file_type("")
-          type.should == nil
-        end
-        it "identify file as polar" do
-          type = Importer.file_type("spec/samples/polarRS200.hrm")
-          type.should == "POLAR_HRM"
-        end
-      end
+	describe "TrackPoint" do
+		context "new trackpoint" do
+			it "has no variables set" do
+				hrm = TrackPoint.new
+				hrm.speed == nil
+				hrm.distance == nil
+				hrm.lat.should == nil
+				hrm.lng.should == nil
+				hrm.altitude.should == nil
+				hrm.hr.should == nil
+			end
+		end
+	end
 
-      context "Parse garmin file" do
-        it "finds workout start time on a short workout" do
-          filename = "spec/samples/indoor-garmin-405.TCX"
-          data = File.read(filename)
-          importer = Importer::Garmin.new(:data => data)
-          workout = importer.restore
-          workout.time.should == Time.parse("Fri Aug 22 01:04:55 UTC 2008")
-        end
-        it "finds the duration on a short workout" do
-          filename = "spec/samples/indoor-garmin-405.TCX"
-          data = File.read(filename)
-          importer = Importer::Garmin.new(:data => data)
-          workout = importer.restore
-          workout.duration.should be_close(755, 1)
-        end
-        it "indoor workout has no trackpoints" do
-          filename = "spec/samples/indoor-garmin-405.TCX"
-          data = File.read(filename)
-          importer = Importer::Garmin.new(:data => data)
-          workout = importer.restore
-          workout.distance.should be_nil
-          workout.average_hr.should be_nil
-          workout.average_speed.should be_nil
-          workout.altitude_gain.should be_nil
-          workout.trackpoints.should == {}
-        end
-        
-        # Parsing the full XML is just slow.  Commenting out for now.
-        it "gets workout level settings for outdoor workout" do
-          filename = "spec/samples/outdoor-garmin-405.TCX"
-          data = File.read(filename)
-          importer = Importer::Garmin.new(:data => data)
-          workout = importer.restore
-          workout.distance.should be_close(11740, 5)
-          workout.average_hr.should be_close(149.7, 0.5)
-          workout.average_speed.should be_close(1.5, 0.2)
-          workout.altitude_gain.should be_close(572, 1.0)
-        end
-        
-        it "doesn't have any 0 in latitude" do
-          filename = "spec/samples/garmin-405-with-0-0.TCX"
-          data = File.read(filename)
-          importer = Importer::Garmin.new(:data => data)
-          workout = importer.restore
-          workout.trackpoints.map {|tp| tp.lat.should_not == 0.0}
-        end
-      end
-      
-      context "Parse polar RS200 file" do
-        it "finds the duration and time" do
-          filename ="spec/samples/polarRS200.hrm"
-          data = File.read(filename)
-          importer = Importer::Polar.new(:data => data, :time_zone => "UTC")
-          workout = importer.restore
-          workout.duration.should be_close(3569,1)
-          workout.time.should == Time.parse("Thu Apr 16 12:01:55 UTC 2009")
-        end
-        it "calculates the average heartrate" do
-          filename ="spec/samples/polarRS200.hrm"          
-          data = File.read(filename)
-          importer = Importer::Polar.new(:data => data, :time_zone => "UTC")
-          workout = importer.restore
-          workout.average_hr.should be_close(145, 1)
-        end
-      end
-      context "Parse a Polar RR file" do
-        it "calculates the heart rate from RR" do
-          filename ="spec/samples/polarRS800-RR.hrm"         
-          data = File.read(filename)
-          importer = Importer::Polar.new(:data => data, :time_zone => "UTC")
-          workout = importer.restore
-          workout.trackpoints.each {|tp| tp.hr.should < 220 && tp.hr.should > 30}
-          workout.average_hr.should be_close(115, 1)
-          workout.average_speed.should == nil
-      end
-    end
-  end
+	describe "Workout" do
+		context "new workout" do
+			it "has no variables set" do
+				workout = Workout.new
+				workout.distance.should == nil
+				workout.duration.should == nil
+				workout.average_hr.should == nil
+				workout.name.should == nil
+				workout.file_name.should == nil
+			end
+			it "set name through initializer" do
+				workout = Workout.new(:name => "test workout")
+				workout.name.should == "test workout"
+			end
+			it "can not set average_hr during init" do
+				workout = Workout.new(:average_hr => 150)
+				workout.average_hr.should == nil
+			end
+		end
+
+		context "Identifies files" do
+			it "identify file as garmin" do
+				type = Importer.file_type("spec/samples/small-garmin.TCX")
+				type.should == "GARMIN_XML"
+			end
+			it "identification returns nil if no file specified" do
+				type = Importer.file_type("")
+				type.should == nil
+			end
+			it "identify file as polar" do
+				type = Importer.file_type("spec/samples/polarRS200.hrm")
+				type.should == "POLAR_HRM"
+			end
+		end
+
+		context "Parse garmin file" do
+			it "finds workout start time on a short workout" do
+				filename = "spec/samples/indoor-garmin-405.TCX"
+				data = File.read(filename)
+				importer = Importer::Garmin.new(:data => data)
+				workout = importer.restore
+				workout.time.should == Time.parse("Fri Aug 22 01:04:55 UTC 2008")
+			end
+			it "finds the duration on a short workout" do
+				filename = "spec/samples/indoor-garmin-405.TCX"
+				data = File.read(filename)
+				importer = Importer::Garmin.new(:data => data)
+				workout = importer.restore
+				workout.duration.should be_close(755, 1)
+			end
+			it "indoor workout has no trackpoints" do
+				filename = "spec/samples/indoor-garmin-405.TCX"
+				data = File.read(filename)
+				importer = Importer::Garmin.new(:data => data)
+				workout = importer.restore
+				workout.distance.should be_nil
+				workout.average_hr.should be_nil
+				workout.average_speed.should be_nil
+				workout.altitude_gain.should be_nil
+				workout.trackpoints.should == {}
+			end
+
+			# Parsing the full XML is just slow.  Commenting out for now.
+			it "gets workout level settings for outdoor workout" do
+				filename = "spec/samples/outdoor-garmin-405.TCX"
+				data = File.read(filename)
+				importer = Importer::Garmin.new(:data => data)
+				workout = importer.restore
+				workout.distance.should be_close(11740, 5)
+				workout.average_hr.should be_close(149.7, 0.5)
+				workout.average_speed.should be_close(1.5, 0.2)
+				workout.altitude_gain.should be_close(583, 1.0)
+			end
+
+			it "gets workout level settings for weird distance workout" do
+				filename = "spec/samples/garmin-405-dies-distance.TCX"
+				data = File.read(filename)
+				importer = Importer::Garmin.new(:data => data)
+				workout = importer.restore
+				workout.distance.should be_close(9426, 1)
+				workout.average_hr.should == nil
+				workout.average_speed.should be_close(6.7, 0.2)
+				workout.altitude_gain.should be_close(40, 1.0)
+			end
+
+			it "doesn't have any 0 in latitude" do
+				filename = "spec/samples/garmin-405-with-0-0.TCX"
+				data = File.read(filename)
+				importer = Importer::Garmin.new(:data => data)
+				workout = importer.restore
+				workout.trackpoints.map {|tp| tp.lat.should_not == 0.0}
+				workout.trackpoints.map {|tp| tp.lat.should_not == "undefined"}
+			end
+		end
+
+		context "Parse polar RS200 file" do
+			it "finds the duration and time" do
+				filename ="spec/samples/polarRS200.hrm"
+				data = File.read(filename)
+				importer = Importer::Polar.new(:data => data, :time_zone => "UTC")
+				workout = importer.restore
+				workout.duration.should be_close(3569,1)
+				workout.time.should == Time.parse("Thu Apr 16 12:01:55 UTC 2009")
+			end
+			it "calculates the average heartrate" do
+				filename ="spec/samples/polarRS200.hrm"          
+				data = File.read(filename)
+				importer = Importer::Polar.new(:data => data, :time_zone => "UTC")
+				workout = importer.restore
+				workout.average_hr.should be_close(145, 1)
+			end
+		end
+		context "Parse a Polar RR file" do
+			it "calculates the heart rate from RR" do
+				filename ="spec/samples/polarRS800-RR.hrm"         
+				data = File.read(filename)
+				importer = Importer::Polar.new(:data => data, :time_zone => "UTC")
+				workout = importer.restore
+				workout.trackpoints.each {|tp| tp.hr.should < 220 && tp.hr.should > 30}
+				workout.average_hr.should be_close(115, 1)
+				workout.average_speed.should == nil
+			end
+		end
+	end
 end
